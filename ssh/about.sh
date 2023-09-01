1bbb817b9d464a2c85cf8c47104462d0caab928c
Microtech001 <130836447+Microtech001@users.noreply.github.com>
2023-09-01 14:30:28 +0800

    XXXX

---
+++ ssh/about.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "#    Premium Auto Script By MTS                 #"
+echo -e "================================================="
+echo -e "# For Debian 9 / 10 / 11 64 bit                 #"
+echo -e "# For Ubuntu 18.04 / Ubuntu 20.04 64 bit        #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# Build Up By MTS                               #"
+echo -e "================================================="
